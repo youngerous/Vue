0f3bb11fb0965b109dc76f3bfa573fea5dbf4447
youngerous <youngerous@gmail.com>
2019-10-04 02:18:49 +0900

    using components

---
+++ vue-cli/src/ServerStatus.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <p>Server Status: {{status}}</p>    
+        <hr>
+        <button @click="changeStatus">Change Status</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            status: 'Critical'
+        }
+    },
+    methods:{
+        changeStatus(){
+            this.status = 'Normal'
+        }
+    }
+}
+</script>
